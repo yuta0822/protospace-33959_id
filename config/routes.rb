0f020932070a8c10b1dc982f1095fc5a494573f8
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-01-26 17:13:02 +0900

    prototypesコントローラーとルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'prototypes/index'
+  root to: "prototypes#index"
 end
--- test/controllers/prototypes_controller_test.rb
+++ test/controllers/prototypes_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PrototypesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get prototypes_index_url
+    assert_response :success
+  end
+
 end
